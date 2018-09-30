06123924ffcd81e0310646e8c0e8678562a1dc66
Jan Hönig <jan.hoenig@fau.de>
2018-09-30 15:21:46 +0200

    Updated screenrc

--- fish/conf.d/10_exports.fish
+++ fish/conf.d/10_exports.fish
@@ -1,4 +1,5 @@
-set -xg PYTHONPATH /home/hrom/git/pystencils $PYTHONPATH
-set -xg PATH /home/hrom/anaconda3/bin $PATH
+#set -xg PYTHONPATH /home/hrom/git/pystencils $PYTHONPATH
+#set -xg PATH /home/hrom/anaconda3/bin $PATH
+set -xg PATH /home/hrom/bin $PATH
 #set -xg LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64 $LD_LIBRARY_PATH
 #set -xg LIBRARY_PATH /usr/local/cuda/lib64/stubs $LIBRARY_PATH
