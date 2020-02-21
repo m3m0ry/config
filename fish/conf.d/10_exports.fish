c2d9fb18f9ff5f990bc6b99dbc7018013ceb1df9
Jan Hönig <jan.hoenig@fau.de>
2020-02-21 23:18:05 +0100

    Updated fish conda config

--- fish/conf.d/10_exports.fish
+++ fish/conf.d/10_exports.fish
@@ -2,7 +2,7 @@
 
 set -xg fish_user_paths $fish_user_paths /home/rzlin/im50ibaq/bin/
 set -xg fish_user_paths $fish_user_paths /home/rzlin/im50ibaq/.local/bin/
-set -xg fish_user_paths $fish_user_paths /local/im50ibaq/anaconda3/bin
+source /home/hrom/anaconda3/etc/fish/conf.d/conda.fish
 
 #set -xg fish_user_paths $fish_user_paths /usr/local/cuda-9.1/bin
 #set -xg LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64 $LD_LIBRARY_PATH
