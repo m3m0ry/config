6e7a21df1d10d5ee38e4d8fe821c87480d9cf7cf
Jan Hönig <jan.hoenig@fau.de>
2020-06-09 18:12:34 +0200

    Updated fisher to an updater script

--- fish/conf.d/10_exports.fish
+++ fish/conf.d/10_exports.fish
@@ -3,8 +3,6 @@ set -xg fish_user_paths $fish_user_paths /home/hrom/.local/bin/
 set -xg fish_user_paths $fish_user_paths /home/hrom/anaconda3/bin/
 source /home/hrom/anaconda3/etc/fish/conf.d/conda.fish
 
-#set -xg PYTHONPATH /local/im50ibaq/git/pystencils $PYTHONPATH
-
 #set -xg fish_user_paths $fish_user_paths /usr/local/cuda-9.1/bin
 #set -xg LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64 $LD_LIBRARY_PATH
 #set -xg LIBRARY_PATH /usr/local/cuda-9.1/lib64/stubs $LIBRARY_PATH
