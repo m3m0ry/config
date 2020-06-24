3e16bc55ac17771dc4fb264cc5e362d1e38fd332
Jan Hönig <jan.hoenig@fau.de>
2020-06-24 17:16:50 +0200

    Removed conda from source

--- fish/conf.d/10_exports.fish
+++ fish/conf.d/10_exports.fish
@@ -1,7 +1,7 @@
 set -xg fish_user_paths $fish_user_paths /home/hrom/bin/
 set -xg fish_user_paths $fish_user_paths /home/hrom/.local/bin/
-set -xg fish_user_paths $fish_user_paths /home/hrom/anaconda3/bin/
-source /home/hrom/anaconda3/etc/fish/conf.d/conda.fish
+#set -xg fish_user_paths $fish_user_paths /home/hrom/anaconda3/bin/
+#source /home/hrom/anaconda3/etc/fish/conf.d/conda.fish
 
 #set -xg fish_user_paths $fish_user_paths /usr/local/cuda-9.1/bin
 #set -xg LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64 $LD_LIBRARY_PATH
