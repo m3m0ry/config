b5d7508674bde6165a6d08976d772129e2a59b87
Jan Hönig <jan.hoenig@fau.de>
2021-01-14 16:55:32 +0100

    Updated fish_prompt and deleted old fisher dependencies

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,3 @@
-function __git_upstream_configured
-    git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
-end
-
 function __print_color
     set -l color  $argv[1]
     set -l string $argv[2]
@@ -11,70 +7,97 @@ function __print_color
     set_color normal
 end
 
-function fish_prompt -d "Simple Fish Prompt"
-    echo -e ""
+function fish_right_prompt -d "Right fishs prompt"
+    if test "$CMD_DURATION" -gt 100
+        __print_color $fish_color_suffix (humantime $CMD_DURATION)
+    end
+
+    __print_color 555 (date "+%H:%M")
+end
+
+function fish_prompt -d "Fish Prompt"
+    # Save our status
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    # Setup prompt_git
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+    if not set -q __fish_git_prompt_color_branch
+        set -g __fish_git_prompt_color_branch magenta --bold
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream informative
+    end
+    if not set -q __fish_git_prompt_char_upstream_ahead
+        set -g __fish_git_prompt_char_upstream_ahead "↑"
+    end
+    if not set -q __fish_git_prompt_char_upstream_behind
+        set -g __fish_git_prompt_char_upstream_behind "↓"
+    end
+    if not set -q __fish_git_prompt_char_upstream_prefix
+        set -g __fish_git_prompt_char_upstream_prefix ""
+    end
+    if not set -q __fish_git_prompt_char_stagedstate
+        set -g __fish_git_prompt_char_stagedstate "●"
+    end
+    if not set -q __fish_git_prompt_char_dirtystate
+        set -g __fish_git_prompt_char_dirtystate "✚"
+    end
+    if not set -q __fish_git_prompt_char_untrackedfiles
+        set -g __fish_git_prompt_char_untrackedfiles "…"
+    end
+    if not set -q __fish_git_prompt_char_invalidstate
+        set -g __fish_git_prompt_char_invalidstate "✖"
+    end
+    if not set -q __fish_git_prompt_char_cleanstate
+        set -g __fish_git_prompt_char_cleanstate "✔"
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+        set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
 
     # User
-    #
-    set -l user (id -un $USER)
-    __print_color FF7676 "$user"
+    __print_color $fish_color_user "$USER"
 
 
     # Host
-    #
-    set -l host_name (hostname -s)
-    set -l host_glyph "@"
-
-    __print_color ffffff "$host_glyph"
-    __print_color F6F49D "$host_name"
-
-
-
+    echo -n "@"
+    __print_color $fish_color_host (prompt_hostname)
 
     # Git
-    #
-    if git_is_repo
-        set -l branch_name (git_branch_name)
-        set -l git_glyph_pre " ["
-        set -l git_glyph_post "] "
-        set -l git_branch_glyph
+    printf '%s ' (fish_git_prompt)
 
-        __print_color ffffff "$git_glyph_pre"
-        __print_color 6597ca "$branch_name"
+    # Newline
+    printf "\e[K\n"
 
-        if git_is_touched
-            if git_is_staged
-                if git_is_dirty
-                    set git_branch_glyph " [±]"
-                else
-                    set git_branch_glyph " [+]"
-                end
-            else
-                set git_branch_glyph " [?]"
-            end
-        end
-
-        __print_color 6597ca "$git_branch_glyph"
-
-        if __git_upstream_configured
-             set -l git_ahead (command git rev-list --left-right --count HEAD...@"{u}" ^ /dev/null | awk '
-                $1 > 0 { printf("⇡") } # can push
-                $2 > 0 { printf("⇣") } # can pull
-             ')
-
-             if test ! -z "$git_ahead"
-                __print_color 5DAE8B " $git_ahead"
-            end
-        end
-		__print_color ffffff "$git_glyph_post"
-    end
-
-
-    __print_color FF7676 "\e[K\n"
     # Current working directory
-    #
-    set -l pwd_string (echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|')
+    __print_color $fish_color_cwd (prompt_pwd)
 
-    __print_color 5DAE8B "$pwd_string"
-    __print_color FF7676 " > "
+    # Print status of last command if not 0
+    set -l pipestatus_string (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+    echo -n $pipestatus_strin
+
+    # Print suffix
+    if functions -q fish_is_root_user; and fish_is_root_user
+      __print_color $fish_color_cwd_root " # "
+    else
+      __print_color $fish_color_suffix " > "
+    end
 end
